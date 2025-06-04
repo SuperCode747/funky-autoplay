7a8307b97f3483717a0268e585d4862e2cfe4f9f
Supercode747 <62219099+SuperCode747@users.noreply.github.com>
2025-06-04 17:34:05 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,6 @@
-print("DOESNT WORK RIGHT IN REWRITE")
+if game.PlaceId == 6447798030 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/SuperCode747/supported/main/FunkyFridayFree'))()
+else
+    local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+    Fluent:Notify({Title = "Uni Hub", Content = "Unsupported Game", Duration = 3})
+end
