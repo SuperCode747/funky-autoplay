1f55a2f4be7476f465cb25cf2b83ba20cf205a41
Supercode747 <62219099+SuperCode747@users.noreply.github.com>
2025-06-04 18:30:46 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,7 @@
 if game.PlaceId == 6447798030 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/SuperCode747/funky-autoplay/refs/heads/main/FunkyFriday.lua'))()
 elseif game.PlaceId == 6520999642 then
-    local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-    local gamename = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-    Fluent:Notify({Title = "Funky Hub", Content = gamename + " 's script not ready yet.", Duration = 3})
+    print("WTDF_")
 else
     local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
     Fluent:Notify({Title = "Funky Hub", Content = "Unsupported Game", Duration = 3})
